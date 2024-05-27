fb33a09e9a5a48d8278fa650ae37d9e156fb46d3
bemnetSeifu <bemnetseifu@gmail.com>
2024-05-28 03:55:47 +0700

    Creating the save feature

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el") 
 
-let countEl = document.getElementById("count-el") // pass in arguments
-
-console.log(countEl)
-
+console.log(saveEl)
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
 
 function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr 
     console.log(count)
 }
