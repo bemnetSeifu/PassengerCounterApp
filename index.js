7b26ede8c0f4d0b5c14df354ac48aeda582fbfd2
bemnetSeifu <bemnetseifu@gmail.com>
2024-05-28 03:26:02 +0700

    Displaying the count and switching button colors

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-document.getElementById("count-el").innerText = 5
+
+let countEl = document.getElementById("count-el") // pass in arguments
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
